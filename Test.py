922a4550e52e176dea7488baefd6a5eda634c414
Onkar <bonecandy.in@gmail.com>
2024-09-20 01:27:25 +0530

    Fix typo and resolve test errors for strikeScore

--- Test.py
+++ Test.py
@@ -1,30 +1,62 @@
-def testGutterGame(self):
-    for i in range(0, 20):
-        self.game.roll(0)  # Change rolls() to roll()
-    assert self.game.score() == 0
+import unittest
+import BowlingGame
 
-def testAllOnes(self):
-    self.rollMany(1, 20)
-    assert self.game.score() == 20
+# Test class for simple cases (Gutter and All Ones)
+class TestSimpleCases(unittest.TestCase):
 
-def testOneSpare(self):
-    self.game.roll(5)
-    self.game.roll(5)
-    self.game.roll(3)
-    self.rollMany(0, 17)
-    assert self.game.score() == 16
+    def setUp(self):
+        self.game = BowlingGame.BowlingGame()
 
-def testOneStrike(self):
-    self.game.roll(10)
-    self.game.roll(4)
-    self.game.roll(3)
-    self.rollMany(0, 16)
-    assert self.game.score() == 24
+    def testGutterGame(self):
+        self.rollMany(0, 20)
+        self.assertEqual(self.game.score(), 0)
 
-def testPerfectGame(self):
-    self.rollMany(10, 12)
-    assert self.game.score() == 300
+    def testAllOnes(self):
+        self.rollMany(1, 20)
+        self.assertEqual(self.game.score(), 20)
 
-def rollMany(self, pins, rolls):
-    for i in range(rolls):
-        self.game.roll(pins)  # Change rolls() to roll()
+    def rollMany(self, pins, rolls):
+        for _ in range(rolls):
+            self.game.roll(pins)
+
+# Test class for bonuses (Spare and Strike)
+class TestBonusCases(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BowlingGame.BowlingGame()
+
+    def testOneSpare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
+        self.rollMany(0, 17)
+        self.assertEqual(self.game.score(), 16)
+
+    def testOneStrike(self):
+        self.game.roll(10)
+        self.game.roll(4)
+        self.game.roll(3)
+        self.rollMany(0, 16)
+        self.assertEqual(self.game.score(), 24)
+
+    def rollMany(self, pins, rolls):
+        for _ in range(rolls):
+            self.game.roll(pins)
+
+# Test class for Perfect Game
+class TestPerfectGame(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BowlingGame.BowlingGame()
+
+    def testPerfectGame(self):
+        self.rollMany(10, 12)
+        self.assertEqual(self.game.score(), 300)
+
+    def rollMany(self, pins, rolls):
+        for _ in range(rolls):
+            self.game.roll(pins)
+
+# Entry point to run all tests
+if __name__ == "__main__":
+    unittest.main()
